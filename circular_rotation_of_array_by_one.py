dbe4841a63b689daeb79491f3b8f15da12433e79
avinashk9911 <90620845+avinashk9911@users.noreply.github.com>
2022-06-07 23:52:49 +0530

    Create circular_rotation_of_array_by_one.py

---
+++ circular_rotation_of_array_by_one.py
@@ -0,0 +1,12 @@
+# circular rotation of array by one
+
+def rotate(array):
+  i=0
+  j=len(array)-1
+  while i!=j:
+    array[i],array[j]=array[j],array[i]
+    i+=1
+  return array
+
+array=[1, 2, 3, 4, 5]
+rotate(array)
